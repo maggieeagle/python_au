1ed52bbe4053d2e7d4057740547d4514d17c77ba
maggieeagle <oryol.margo@gmail.com>
2020-10-17 12:11:07 +0300

    LEETCODE Added 2 tasks

--- leetcode/generate_md.py
+++ leetcode/generate_md.py
@@ -9,21 +9,18 @@ page = md.read()
 
 tcode = src.read().split("\n")
 
-title = tcode[0][tcode[0].find(".") + 2:-1]
+title = tcode[0][tcode[0].find(".") + 2:]
 
 link = tcode[1]
 sublink = link[link.rfind("problems/"):-1]
 
 solution = ""
 for i in range(3, len(tcode)):
-    tcode[i] = tcode[i][7:] + "\n"
+    tcode[i] = tcode[i][3:] + "\n"
     solution += tcode[i]
 
 page = page.replace("##", "+ [" + title + "](#" + sublink + ")\n\n##", 1)
 md.seek(0)
 md.write(page)
 
-md.write(" " + title + "\n\n" + link + "\n\n```Python\n" + solution + "```\n\n##")
-
-src.close()
-md.close()
+md.write(" " + title + "\n\n" + link + "\n\n```python\n" + solution + "```\n\n##")
