4a07d24a79c44a0ed6ac1a7cfd7f043251e2f5dc
maggieeagle <oryol.margo@gmail.com>
2021-03-16 21:02:10 +0300

    GENERATOR 1022 Returned generator to normal

--- leetcode/generate_md.py
+++ leetcode/generate_md.py
@@ -1,42 +1,29 @@
-import sys
+src = open('source_leetcode_data.txt', 'r')
+md = open('math.md', 'r+')
 
-def readFile(file_in):
+if (md.read(1) != "#"):
+    md.write("# Intervals\n\n\n##")
 
-    src = open(file_in, 'r')
-    code = src.read().split("\n")
-    title = code[0][code[0].find(".") + 2:]
+md.seek(0)
+page = md.read()
 
-    link = code[1]
-    sublink = link[link.rfind("problems/"):10]
+tcode = src.read().split("\n")
 
-    solution = ""
-    for i in range(3, len(code)):
-        code[i] = code[i][3:] + "\n"
-        solution += code[i]
-    return " " + title + "\n\n" + link + "\n\n```python\n" + solution + "```\n\n##"
+title = tcode[0][tcode[0].find(".") + 2:]
 
-    src.close()
+link = tcode[1]
+sublink = link[link.rfind("problems/"):-1]
 
-def writeFile(file_out, text):
-    md = open('intervals.md', 'r+')
-    if (md.read(1) != "#"):
-        md.write("# Intervals\n\n\n##")
+solution = ""
+for i in range(3, len(tcode)):
+    tcode[i] = tcode[i][3:] + "\n"
+    solution += tcode[i]
 
-    md.seek(0)
-    page = md.read()
+page = page.replace("##", "+ [" + title + "](#" + sublink + ")\n\n##", 1)
+md.seek(0)
+md.write(page)
 
-    page = page.replace("##", "+ [" + title + "](#" + sublink + ")\n\n##", 1)
-    md.seek(0)
-    md.write(page)
+md.write(" " + title + "\n\n" + link + "\n\n```python\n" + solution + "```\n\n##")
 
-    md.write(text)
-
-    md.close()
-
-def main(file_in, file_out):
-
-    writeFile(file_out, readFile(file_in))
-
-if __name__ == "__main__":
-    param = sys.argv
-    main(param[1], param[2])
+src.close()
+md.close()
